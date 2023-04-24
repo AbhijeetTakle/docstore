a65fef0fc24d0807bed39c6248d31728a4c9e98f
Abhijeet Takle <abhijeettakle35@gmail.com>
2023-04-24 18:49:28 +0530

    Frontend for Docstore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
+import CLIENT_ID from "./utils";
 
 function App() {
+  const [user, setUser] = useState();
+  const [file, setFile] = useState();
+  const [docs, setDocs] = useState();
+
+  function handleCallbackResponse(response) {
+    const usr = jwtDecode(response.credential);
+    setUser(usr);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: CLIENT_ID,
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
+  useEffect(() => {
+    function getDocuments() {
+      if (user !== undefined) {
+        fetch(`http://localhost:5000/docs/${user.email}`, {
+          method: "GET",
+        })
+          .then((res) => res.json())
+          .then((response) => {
+            setDocs(response.docs);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+    getDocuments();
+  }, [user]);
+
+  useEffect(() => {
+    const signin = document.getElementById("signInDiv");
+    signin.setAttribute("display", "none");
+
+    if (user !== undefined) {
+      let formdata = new FormData();
+      const data = JSON.stringify({
+        email: user.email,
+      });
+      formdata.append("email", user.email);
+
+      fetch("http://localhost:5000/user", {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((response) => {})
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [user]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formdata = new FormData();
+    formdata.append("file", file);
+    formdata.append("email", user.email);
+
+    fetch("http://localhost:5000/files", {
+      method: "POST",
+      body: formdata,
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        setDocs([...docs, response]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        id="signInDiv"
+        style={{
+          display: user === undefined ? "block" : "none",
+        }}
+      ></div>
+      <div
+        id="mainpage"
+        style={{
+          display: user === undefined ? "none" : "block",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <input
+            id="choosefile"
+            type="file"
+            onChange={handleFileChange}
+          ></input>
+          <input id="submitbtn" type="submit"></input>
+        </form>
+        <div id="alldocs">
+          {docs === undefined
+            ? ""
+            : docs.map((doc, idx) => {
+                return (
+                  <a className="listitem" href={doc.link} key={idx}>
+                    {doc.name}
+                  </a>
+                );
+              })}
+        </div>
+      </div>
     </div>
   );
 }
